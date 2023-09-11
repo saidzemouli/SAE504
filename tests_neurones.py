8d56e99539ef60195360a44d3aa3cc8074ce8364
Saïd Zemouli <said.zemouli@iut-dijon.u-bourgogne.fr>
2023-09-11 22:40:33 +0200

    Création de test_return_float_coefficients et test_length_coefficients

--- tests_neurones.py
+++ tests_neurones.py
@@ -2,15 +2,28 @@
 # encoding: utf-8
 
 import unittest
-from neurones import Neurone
+from neurones import *
 
 class TestNeurone(unittest.TestCase):
-    
-    def test_coefficients(self):
-        neurone = Neurone(7)
-        coefficient = neurone.coefficients[0]
-        is_float = isinstance(coefficient, float)
-        self.assertTrue(is_float)
+    def test_return_float_coefficients(self):
+        # Créez une instance de Neurone avec un nombre arbitraire d'entrées
+        nombre_entrees = 7
+        neurone = Neurone(nombre_entrees)
+
+        # Accédez aux coefficients à partir de l'instance de Neurone
+        coefficients = neurone.coefficients
+
+        # Vérifiez que chaque coefficient est un nombre à virgule flottante (float)
+        for coefficient in coefficients:
+            self.assertTrue(isinstance(coefficient, float))
+
+    def test_length_coefficients(self):
+
+        nombre_entrees = 7
+        neurone = Neurone(nombre_entrees)
+        coefficients = neurone.coefficients
+        # Vérifie que la longueur de la liste coefficients est égale au nombre d'entrées
+        self.assertEqual(len(coefficients), nombre_entrees)
 
 if __name__ == '__main__':
     unittest.main()
