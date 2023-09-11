9963628c46affa9bf2e13070d87faf1495830471
Saïd Zemouli <said.zemouli@iut-dijon.u-bourgogne.fr>
2023-09-11 21:37:17 +0200

    Test de la création des coefficients => OK

---
+++ tests_neurones.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# encoding : utf -8
+
+import unittest
+from neurones import Neurone
+
+# Test de la création des coefficients
+Neurone(7)
+
+if __name__ == '__main__' :
+    unittest.main()
