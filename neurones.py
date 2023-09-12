b5c9504e2694f7b15653a9d886f50dca6ce96917
Saïd Zemouli <said.zemouli@iut-dijon.u-bourgogne.fr>
2023-09-12 08:36:40 +0200

    Ajout de commentaires dans le fichier neurones.py

--- neurones.py
+++ neurones.py
@@ -2,7 +2,9 @@ import random
 
 class Neurone:
     def __init__(self, nombre_entrees):
+        # Vérifie que le nombre d'entrées est supérieur à zéro
         if nombre_entrees <= 0:
-            raise ValueError("Le nombre d'entrées doit être supérieur à zéro.")
+            raise ValueError("Le nombre d'entrées doit être supérieur à zéro.") 
 
-        self.coefficients = [random.uniform(-1, 1) for _ in range(nombre_entrees)]
+        # Création de la liste coefficients
+        self.coefficients = [random.uniform(-1, 1) for _ in range(nombre_entrees)] 
