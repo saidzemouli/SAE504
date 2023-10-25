cdce3ea12f17531bf5d0d55efd5d0cd9e249cdaf
Saïd Zemouli <said.zemouli@iut-dijon.u-bourgogne.fr>
2023-10-25 11:56:27 +0200

    Maj du fichier neurones.py pour modification de la classe Learning

--- PremierNeurone.py
+++ PremierNeurone.py
@@ -2,29 +2,29 @@ import random
 import matplotlib.pyplot as plt
 from neurones import * 
 
-if __name__ == "__main":
-    entrees = [
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 0, 0, 0, 0],
-        [1, 1, 0, 1, 1, 0, 1],
-        [1, 1, 1, 1, 0, 0, 1],
-        [1, 1, 1, 0, 0, 1, 1],
-        [1, 0, 1, 1, 0, 1, 1],
-        [1, 0, 1, 1, 1, 1, 1],
-        [1, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 0, 1, 1]
-    ]
 
-    sortie = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+entrees = [
+    [1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0],
+    [1, 1, 0, 1, 1, 0, 1],
+    [1, 1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 0, 0, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1]
+]
 
-    neurone = Neurone(7)
-    learning = Learning(neurone, entrees, sortie)
+sortie = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
 
-    erreurs = learning.apprendreSimple(1000)
+neurone = Neurone(7)
+learning = Learning(neurone, entrees, sortie)
 
-    plt.plot(erreurs)
-    plt.xlabel('Epochs')
-    plt.ylabel('Erreur moyenne')
-    plt.title('Évolution de l\'erreur moyenne')
-    plt.show()
+erreurs = learning.apprendreSimple(1000)
+
+plt.plot(erreurs)
+plt.xlabel('Epochs')
+plt.ylabel('Erreur moyenne')
+plt.title('Évolution de l\'erreur moyenne')
+plt.show()
--- neurones.py
+++ neurones.py
@@ -1,112 +1,132 @@
-import random
 import math
+import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Neurone:
-
     def __init__(self, nombre_entrees):
-        if not isinstance(nombre_entrees, int):
-            raise ValueError("Le nombre d'entrées doit être un nombre entier.")
-        elif nombre_entrees <= 0:
+        if nombre_entrees <= 0:
             raise ValueError("Le nombre d'entrées doit être supérieur à zéro.")
-        else:
-            self.__nombre_entrees = nombre_entrees
-            # Création de la liste coefficients
-            self.__coefficients = [random.uniform(-1, 1) for _ in range(nombre_entrees + 1)]
-
-    def getNeuronSize(self): # Méthode pour obtenir le nombre d'entrées
-        return self.__nombre_entrees
-    
-    def getCoefficient(self, index):
-        if not isinstance(index, int):
-            raise ValueError("L'index doit être un nombre entier.")
-        if index < 0 or index > self.__nombre_entrees:
-            raise ValueError("L'index doit être compris entre 0 et le nombre d'entrées.")
-        else:
-            return self.__coefficients[index]
         
-    def setCoefficient(self, valeur, index):
-        if not isinstance(index, int):
-            raise ValueError("L'index doit être un nombre entier.")
-        if index < 0 or index > self.__nombre_entrees:
-            raise ValueError("L'index doit être compris entre 0 et le nombre d'entrées.")
-        else:
-            self.__coefficients[index] = valeur
+        # Initialisation aléatoire des coefficients entre -1 et 1
+        self.coefficients = [random.uniform(-1, 1) for _ in range(nombre_entrees + 1)]
 
-    def getOutput(self, liste):
-        output = 0
-        if not isinstance(liste, list):
-            raise ValueError("La liste doit être une liste.")
-        if len(liste) != self.__nombre_entrees:
-            raise ValueError("La liste doit avoir le même nombre d'entrées que le neurone.")
-        if not all(isinstance(x, float) for x in liste):
-            raise ValueError("Les valeurs de la liste doivent être des nombres à virgule flottante (float).")
-        else:
-            for i in range(self.__nombre_entrees):
-                output += liste[i] * self.__coefficients[i]
-                #print(liste[i], " x ", self.__coefficients[i], " = ", liste[i] * self.__coefficients[i])
-            #print("output = ", output)
-            output += self.__coefficients[self.__nombre_entrees]
-            #print(self.__coefficients[self.__nombre_entrees])
-            #print("output = ", output)
-            return output
-
-def sigmoid(x):
-    if x > 100 :
-        return(1.0)
-    elif x < -100 :
-        return(0.0)
-    else :
-        return(1.0 / (1.0 + math.exp(-x)))
+    def getNeuronSize(self):
+        # Nombre d'entrées
+        return len(self.coefficients) - 1
     
+    def getCoefficient(self, position):
+        # Retourne la valeur du coefficient à la position donnée
+        if 0 <= position < len(self.coefficients):
+            return self.coefficients[position]
+        else:
+            raise ValueError("Position de coefficient invalide.")
+
+    def setCoefficient(self, position, valeur):
+        # Modifie la valeur du coefficient à la position donnée
+        if 0 <= position < len(self.coefficients):
+            self.coefficients[position] = valeur
+        else:
+            raise ValueError("Position de coefficient invalide.")
+        
+    def getOutput(self, entrees):
+        # Calcule la sortie du neurone pour la configuration donnée.
+        if len(entrees) != len(self.coefficients) - 1:
+            raise ValueError("Le nombre d'entrées ne correspond pas à la taille du neurone.")
+        
+        somme_ponderee = sum(entrees[i] * self.coefficients[i] for i in range(len(entrees)))
+        return somme_ponderee
+
+def sigmoid(a):
+    if a > 100:
+        return 1.0
+    if a < -100:
+        return 0.0
+    else:
+        return 1 / (1 + math.exp(-a))
+
 class SigmoidNeuron(Neurone):
-    def __init__(self, nombre_entrees):
-        super().__init__(nombre_entrees)
-    
-    def calculeSortie(self, liste):
-        return sigmoid(super().getOutput(liste))
-    
+    def __init__(self, entre):
+        Neurone.__init__(self, entre)
 
+    def calculerSortie(self, num):
+        return sigmoid(super().getOutput(num))
+                    
 class Learning:
     def __init__(self, neurone, entrees, sortie):
+        if len(entrees) != len(sortie):
+            raise ValueError("Le nombre d'entrées ne correspond pas au nombre de sortie.")
+        for i in range(len(entrees)): 
+            if len(entrees[i]) != neurone.getNeuronSize():
+                raise ValueError("Le nombre d'entrées ne correspond pas à la taille du neurone.")
         self.neurone = neurone
         self.entrees = entrees
-        self.sortie = [sortie]
+        self.sortie = sortie
 
-    def calculerErreur(self, entree, sortie):
-            prediction = self.neurone.calculeSortie(entree)
-            erreur = (sortie - prediction) ** 2
-            return erreur
+    def calculerErreur(self, index):
+        # Calculer la sortie du neurone pour le jeu d'entrées 'entrees[index]'.
+        neurone_sortie = self.neurone.calculerSortie(self.entrees[index])
+        # Calculer l'erreur en carré.
+        erreur = (neurone_sortie - self.sortie[index]) ** 2
+        return erreur
 
     def calculerErreurMoyenne(self):
-        total_erreur = 0
-        for i in range(len(self.entrees)):
-            total_erreur += self.calculerErreur(self.entrees[i], self.sortie[i])
+        # Calculer la moyenne des erreurs pour tous les éléments de 'entrees' et 'sortie'.
+        total_erreur = sum(self.calculerErreur(index) for index in range(len(self.entrees)))
         erreur_moyenne = total_erreur / len(self.entrees)
         return erreur_moyenne
-    
-    def apprendreSimple(self, epochs=1000):
-        erreurs = []
 
-        for epoch in range(epochs):
+    def apprendreSimple(self, epochs=1000):
+        erreurs = []  # Stocker l'évolution de l'erreur moyenne.
+        for _ in range(epochs):
             erreur_actuelle = self.calculerErreurMoyenne()
             erreurs.append(erreur_actuelle)
-
-            # Choisir un coefficient au hasard dans le neurone
-            coefficient_index = random.randint(0, self.neurone.getNeuronSize())
-            ancienne_valeur = self.neurone.getCoefficient(coefficient_index)
-
-            # Ajoutez une valeur aléatoire entre -0.1 et +0.1 au coefficient
-            delta = random.uniform(-0.1, 0.1)
-            self.neurone.setCoefficient(ancienne_valeur + delta, coefficient_index)
-
-            # Recalcule del'erreur moyenne
+            # Choisir un coefficient au hasard dans le neurone.
+            random_coefficient_index = random.randint(0, len(self.neurone.coefficients) - 1)
+            old_coefficient = self.neurone.coefficients[random_coefficient_index]
+            # Ajouter une valeur aléatoire entre -0.1 et +0.1.
+            random_change = random.uniform(-0.1, 0.1)
+            new_coefficient = old_coefficient + random_change
+            # Mettre à jour le coefficient dans le neurone.
+            self.neurone.coefficients[random_coefficient_index] = new_coefficient
+            # Recalculer l'erreur moyenne.
             nouvelle_erreur = self.calculerErreurMoyenne()
-
-            # Si la nouvelle erreur est plus grande, remettre l'ancienne valeur du coefficient
+            # Si la nouvelle erreur est plus élevée, remettre l'ancienne valeur au coefficient.
             if nouvelle_erreur > erreur_actuelle:
-                self.neurone.setCoefficient(ancienne_valeur, coefficient_index)
+                self.neurone.coefficients[random_coefficient_index] = old_coefficient
 
         return erreurs
     
- 
+    def apprendreAvecMemoire(self, epochs=1000):
+        erreurs = []  # Stocker l'évolution de l'erreur moyenne.
+        coefficients_initiaux = list(self.neurone.coefficients)
+        modification = 0.1
+        signe_modification = True
+
+        for _ in range(epochs):
+            erreur_actuelle = self.calculerErreurMoyenne()
+            erreurs.append(erreur_actuelle)
+            # Choisir un coefficient au hasard dans le neurone.
+            random_coefficient_index = random.randint(0, len(self.neurone.coefficients) - 1)
+            old_coefficient = self.neurone.coefficients[random_coefficient_index]
+            # Changer la valeur du coefficient avec le signe fixé par le booléen et la valeur de modification.
+            if signe_modification:
+                self.neurone.coefficients[random_coefficient_index] += modification
+            else:
+                self.neurone.coefficients[random_coefficient_index] -= modification
+            # Stocker la valeur du coefficient avant la modification.
+            new_coefficient = self.neurone.coefficients[random_coefficient_index]
+            # Recalculer l'erreur moyenne.
+            nouvelle_erreur = self.calculerErreurMoyenne()
+            # Si la nouvelle erreur est plus élevée ou égale, remettre la valeur du coefficient à l'ancienne.
+            if nouvelle_erreur >= erreur_actuelle:
+                self.neurone.coefficients[random_coefficient_index] = old_coefficient
+                # Choisir un nouveau coefficient, une nouvelle valeur et un nouveau signe.
+                random_coefficient_index = random.randint(0, len(self.neurone.coefficients) - 1)
+                modification = random.uniform(0, 0.1)
+                signe_modification = random.choice([True, False])
+            else:
+                # Augmenter la valeur absolue de la modification de 10%.
+                modification *= 1.1
+
+        return erreurs
